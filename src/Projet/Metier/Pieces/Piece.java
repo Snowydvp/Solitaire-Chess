e9d5be384ca2c649f4082e08d22f08d5ed4f3ea3
Ashbringer76 <judokadu76@hotmail.fr>
2016-06-13 11:23:08 +0200

    test1

--- src/Projet/Metier/Pieces/Piece.java
+++ src/Projet/Metier/Pieces/Piece.java
@@ -1,9 +1,12 @@
 package Projet.Metier.Pieces;
 
+import Projet.Metier.Plateau;
+
 public abstract class Piece 
 {
 	protected int posX;
 	protected int posY;
+	protected Plateau pl;
 	
 	public Piece(int posX, int posY)
 	{
@@ -13,6 +16,8 @@ public abstract class Piece
 	
 	public int getPosX(){return this.posX;}
 	public int getPosY(){return this.posY;}
+	public void setPosX(int x){this.posX = x;}
+	public void setPosY(int y){this.posY = y;}
 	
 	public abstract boolean mouvement(int x, int y);
 	public abstract char    getType();
