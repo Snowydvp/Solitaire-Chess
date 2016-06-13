2fa325c0c8b47ec7ca0c24fbe60701e6dac7cb75
Ashbringer76 <judokadu76@hotmail.fr>
2016-06-13 14:13:12 +0200

    t

--- src/Projet/Metier/Pieces/Cavalier.java
+++ src/Projet/Metier/Pieces/Cavalier.java
@@ -7,14 +7,18 @@ public class Cavalier extends Piece
 	{
 	}
 
-	@Override
-	public boolean  mouvement(int deltaX, int deltaY)
+	public boolean mouvement(int deltaX, int deltaY) 
 	{
-		// TODO Auto-generated method stub
+		if(deltaX == -2 || deltaX == 2)
+			if (deltaY == -1 || deltaY == 1)
+				return true;
+
+		if(deltaY == -2 || deltaY == 2)
+			if (deltaX == -1 || deltaX == 1)
+				return true;
 		return false;
 	}
-
-
+		
 	public char getType() {return 'C';}
 
 
