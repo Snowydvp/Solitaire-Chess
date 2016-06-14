468d9d67d1a5063b13cf33ee100bc46c11078ef2
Snowy <Snowy@172.30.13.151>
2016-06-14 09:10:25 +0200

    pieces capturees

--- src/Projet/Metier/Plateau.java
+++ src/Projet/Metier/Plateau.java
@@ -2,34 +2,38 @@ package Projet.Metier;
 
 
 
+import java.util.ArrayList;
+
 import Projet.Metier.Pieces.Piece;
 
 public class Plateau
 {
 	private Piece[][]        tabPiece;
+	private ArrayList<Piece> capturees;
 	
 	public Plateau(Piece[][] tabPiece)
 	{
 		this.tabPiece = new Piece[4][4];
-		
+		this.capturees = new ArrayList<>();
 		this.tabPiece = tabPiece;
 	}
 	
-	public Piece deplacer(Piece p, int cibleX, int cibleY)
+	public boolean deplacer(Piece p, int cibleX, int cibleY)
 	{
 		if(p.deplacementValide(p.getPosX()-cibleX, p.getPosY()-cibleY) && this.tabPiece[cibleX][cibleY] != null)
 		{
-			Piece capturee = tabPiece[cibleX][cibleY];
 			tabPiece[p.getPosX()][p.getPosY()] = null;
+			this.capturees.add(tabPiece[cibleX][cibleY]);
 			tabPiece[cibleX][cibleY] = p;
 			p.setPosX(cibleX);
 			p.setPosY(cibleY);
-			return capturee;
+			return true;
 		}
-		return null;
+		return false;
 	}
 	
 	public Piece[][] getTabPiece(){return this.tabPiece;}
+	public ArrayList<Piece> getPiecesCapturees(){return this.capturees;}
 	
 	public String toString()
 	{
