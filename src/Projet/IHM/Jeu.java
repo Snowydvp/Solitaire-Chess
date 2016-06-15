bd0b83ca4bc16240ed393dcdb2dc16fe6b0c9e41
Maxence <Maxence@172.26.6.249>
2016-06-15 09:59:07 +0200

    ghhg

--- src/Projet/IHM/Jeu.java
+++ src/Projet/IHM/Jeu.java
@@ -1,23 +1,28 @@
 package Projet.IHM;
 
+import Projet.Controleur;
+import Projet.IHM.Fenetre;
+import Projet.Metier.Pieces.*;
+
+import java.util.ArrayList;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.ArrayList;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
-import Projet.Controleur;
-import Projet.Metier.Pieces.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.plaf.IconUIResource;
 
 /**
  * Créé par BELLANGER Jessy, LINTOT Maxime, PICOT Maxence et SINAEVE Antoine le 14/06/2016.
